598af460cc695506bff4c1ea0b7fa2778da12f85
sso9594 <sso9594@naver.com>
2023-10-30 23:27:51 +0900

    solve problem 64~70

---
+++ problem61~80/problem68.py
@@ -0,0 +1,11 @@
+n = int(input())
+if (n/10)>=9:
+    print('A')
+else:
+    if(n/10)>=7:
+        print('B')
+    else:
+        if(n/10)>=4:
+            print("C")
+        else:
+            print('D')
---
+++ problem61~80/problem69.py
@@ -0,0 +1,14 @@
+a = str(input())
+if(a=='A'):
+    print('best!!!')
+else:
+    if(a=='B'):
+        print('good!!')
+    else:
+        if(a=='C'):
+            print('run!')
+        else:
+            if(a=='D'):
+                print('slowly~')
+            else:
+                print('what?')
